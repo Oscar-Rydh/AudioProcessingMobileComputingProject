c97b49c99701306fbb48ab387c3cf564f10545f0
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-05-27 16:13:03 +0900

    Sound file now changes the name

--- http_server/index.js
+++ http_server/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 var multer = require('multer')
-var upload = multer({dest: 'uploads/sound.wav'});
+var upload = multer({dest: 'uploads/'});
 var app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -11,7 +11,16 @@ const { exec } = require('child_process')
 app.post('/estimation', upload.single('file'), function (req, res) {
     console.log(req.file)
     console.log(req.body)
-    const estimate = exec('pwd');
+    var fileName = req.file.filename;
+    console.log(fileName)
+
+    const pwd = exec('pwd');
+    pwd.stdout.on('data', data => {
+        console.log(data)
+    })
+
+
+    const estimate = exec('mv ./uploads/' + fileName + " ./uploads/sound.wav");
 
     estimate.stdout.on('data', data => {
         console.log("Shell returned:\n " + data);
@@ -19,6 +28,7 @@ app.post('/estimation', upload.single('file'), function (req, res) {
             result: data
         })
     })
+    res.status(200).send()
 
 })
 
