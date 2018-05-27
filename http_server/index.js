be8b5805ffb2627f409c7cdbdf35da2e2ae9a234
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-05-27 16:03:32 +0900

    Working upload on the http server

--- http_server/index.js
+++ http_server/index.js
@@ -1,9 +1,16 @@
 var express = require('express')
+var bodyParser = require('body-parser');
+var multer = require('multer')
+var upload = multer({dest: 'uploads/sound.wav'});
 var app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 const { exec } = require('child_process')
 
-app.get('/estimation', function (req, res) {
 
+app.post('/estimation', upload.single('file'), function (req, res) {
+    console.log(req.file)
+    console.log(req.body)
     const estimate = exec('pwd');
 
     estimate.stdout.on('data', data => {
