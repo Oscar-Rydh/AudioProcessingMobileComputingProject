6accd1954bcd3c1cbf99109c95ef9a6d4669906a
Joel Klint <joel.klint@gmail.com>
2018-05-30 12:46:03 +0900

    Tried retraining model to look on individual dimensions

--- ML_MLP.R
+++ ML_MLP.R
@@ -67,38 +67,72 @@ prepareData <- function() {
 x_train <- prepareData()
 str(x_train)
 
-# Volume
-y_train <- array(c(rep(71.2, 12), 
-                   rep(92.0, 12), 
-                   rep(57.9, 12), 
-                   rep(15.7, 12), 
-                   #rep(1.2, 2),
-                   rep(138.2, 12), 
-                   rep(24.9, 12),
-                   rep(2.9, 12), 
-                   rep(103.4, 12), 
-                   rep(18.8, 12),
-                   rep(8.4, 12),
-                   rep(8.1, 12),
-                   rep(18.7, 12)
+# # Volume
+# y_train <- array(c(rep(71.2, 12), 
+#                    rep(92.0, 12), 
+#                    rep(57.9, 12), 
+#                    rep(15.7, 12), 
+#                    #rep(1.2, 2),
+#                    rep(138.2, 12), 
+#                    rep(24.9, 12),
+#                    rep(2.9, 12), 
+#                    rep(103.4, 12), 
+#                    rep(18.8, 12),
+#                    rep(8.4, 12),
+#                    rep(8.1, 12),
+#                    rep(18.7, 12)
+# )
+# )
+# Individual dimensions
+y_train <- array(c(rep(40.5, 6),
+                   rep(52.5, 3),
+                   rep(33.5, 3),
+                   
+                   rep(46.0, 6),
+                   rep(46.0, 3),
+                   rep(43.5, 3),
+                   
+                   rep(31.5, 6),
+                   rep(52.5, 3),
+                   rep(35.0, 3),
+                   
+                   rep(19.0, 6),
+                   rep(33.0, 3),
+                   rep(25.0, 3),
+                   
+                   rep(79.0, 6),
+                   rep(53.0, 3),
+                   rep(33.0, 3),
+                   
+                   rep(24.0, 6),
+                   rep(37.0, 3),
+                   rep(28.0, 3),
+                   
+                   rep(8.0, 6),
+                   rep(24.0, 3),
+                   rep(15.0, 3),
+                   
+                   rep(40.0, 6),
+                   rep(55.0, 3),
+                   rep(47.0, 3),
+                   
+                   rep(20.0, 6),
+                   rep(33.5, 3),
+                   rep(28.0, 3),
+                   
+                   rep(24.0, 6),
+                   rep(22.0, 3),
+                   rep(16.0, 3),
+                   
+                   rep(12.0, 6),
+                   rep(32.0, 3),
+                   rep(21.0, 3),
+                   
+                   rep(16.0, 6),
+                   rep(39.0, 3),
+                   rep(30.0, 3)
+                   )
 )
-)
-# Height
-# y_train <- array(c(rep(40.5, 6), 
-#                    rep(46.0, 6), 
-#                    rep(31.5, 6),
-#                    rep(19.0, 6), 
-#                    rep(7.0, 2),
-#                    rep(79.0, 6), 
-#                    rep(24.0, 6), 
-#                    rep(8.0, 6), 
-#                    rep(40.0, 6), 
-#                    rep(20.0, 6),
-#                    rep(24.0, 6),
-#                    rep(12.0, 6),
-#                    rep(16.0, 6)
-#                    )
-#)
 
 
 
@@ -132,20 +166,20 @@ data <- train_test_split()
 
 # Params -----
 
-batch_size <- 15
-epochs <- 25
+batch_size <- 20
+epochs <- 15
 # Define Model --------------------------------------------------------------
 
 model <- keras_model_sequential()
 model %>% 
-  layer_dense(units = 2000, activation = 'relu', input_shape = c(69632)) %>% 
-  # layer_dropout(rate = 0.3) %>%
-  layer_dense(units = 1300, activation = 'relu') %>%
-  # layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 3000, activation = 'relu', input_shape = c(69632)) %>% 
+  layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 1200, activation = 'relu') %>%
+  layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 300, activation = 'relu') %>%
+  layer_dropout(rate = 0.2) %>%
   layer_dense(units = 50, activation = 'relu') %>%
-  # layer_dropout(rate = 0.3) %>%
-  #layer_dense(units = 10, activation = 'relu') %>%
-  # layer_dropout(rate = 0.3) %>%
+  layer_dropout(rate = 0.2) %>%
   layer_dense(units = 1, activation = 'linear')
 
 summary(model)
@@ -182,11 +216,11 @@ matrix(data$test$y)
 round(array(abs(predicted - matrix(data$test$y))) / array(predicted), 2)
 
 # Save model
-test_type <- 'volume'
-#test_type <- 'height'
+#test_type <- 'volume'
+test_type <- 'length'
 filename <- paste("models/", test_type, "-loss-", round(score$loss, 3), 
                   "-meanAbsError-", round(score$mean_absolute_error, 3),
-                  ".hdf5", sep="")
+                  ".hdf5", sep=""); filename
 save_model_hdf5(model, filename, overwrite = FALSE, include_optimizer = TRUE)
 
 
